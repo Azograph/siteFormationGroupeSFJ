92672f4edd493cf6dc5245cb89bdf8c43bc16f3b
Azograph <50628970+Azograph@users.noreply.github.com>
2026-04-30 12:15:40 +0200

    FEAT dossier julietteBranch

---
+++ julietteBranch/script.js
@@ -0,0 +1,13 @@
+
+
+$(function(){
+    $('.deplie, .actif').hide(); // on masque tout 
+    $('.bouton-deplier').click(function(event){
+        event.preventDefault(); // on ne suit pas le lien
+        $(this).parent().next().slideToggle();
+    });
+    $('.bouton-replier').click(function(event){
+        event.preventDefault(); // on ne suit pas le lien
+        $(this).parent().slideToggle();
+    });
+});
